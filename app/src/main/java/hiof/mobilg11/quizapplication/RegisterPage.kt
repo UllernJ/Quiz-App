234a52cfc40abaa226d30baa181b3f24c8fae7f0
UllernJ <Ulrik.johannesen@gmail.com>
2023-09-19 13:15:45 +0200

    sample register page

---
+++ app/src/main/java/hiof/mobilg11/quizapplication/RegisterPage.kt
@@ -0,0 +1,12 @@
+package hiof.mobilg11.quizapplication
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterPage() {
+    Column {
+        Text(text = "UWU")
+    }
+}
