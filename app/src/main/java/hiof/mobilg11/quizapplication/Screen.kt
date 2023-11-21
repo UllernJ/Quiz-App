f58b79e93e66ba1bd6ad2543eef48c095a6128cf
UllernJ <Ulrik.johannesen@gmail.com>
2023-11-22 00:22:42 +0100

    Added source

--- app/src/main/java/hiof/mobilg11/quizapplication/Screen.kt
+++ app/src/main/java/hiof/mobilg11/quizapplication/Screen.kt
@@ -1,5 +1,10 @@
 package hiof.mobilg11.quizapplication
 
+/*
+Inspired by : https://stackoverflow.com/questions/69686087/why-use-sealed-class-and-make-object-in-navigation-kotlin-jetpack-compose
+ */
+
+
 const val QUIZ_ARGUMENT_KEY = "categoryName"
 const val LOBBY_ARGUMENT_KEY = "lobbyId"
 
@@ -13,17 +18,20 @@ sealed class Screen(val route: String) {
             return this.route.replace("{$QUIZ_ARGUMENT_KEY}", categoryName)
         }
     }
+
     data object Multiplayer : Screen("multiplayer")
     data object MultiplayerLobby : Screen("multiplayer/{$LOBBY_ARGUMENT_KEY}") {
         fun createRoute(lobbyId: String): String {
             return this.route.replace("{$LOBBY_ARGUMENT_KEY}", lobbyId)
         }
     }
+
     data object MultiplayerPlay : Screen("multiplayer/play/{$LOBBY_ARGUMENT_KEY}") {
         fun createRoute(lobbyId: String): String {
             return this.route.replace("{$LOBBY_ARGUMENT_KEY}", lobbyId)
         }
     }
+
     data object SinglePlayer : Screen("singleplayer")
     data object Loading : Screen("loading")
     data object Notifications : Screen("notifications")
