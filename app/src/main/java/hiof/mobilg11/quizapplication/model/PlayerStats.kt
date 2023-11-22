100322e3a13ac00b6242d184c0be3bdc77450569
Peter Brännström <peterjbrannstrom@gmail.com>
2023-11-22 23:19:11 +0100

    Minor changes

--- app/src/main/java/hiof/mobilg11/quizapplication/model/PlayerStats.kt
+++ app/src/main/java/hiof/mobilg11/quizapplication/model/PlayerStats.kt
@@ -1,9 +1,19 @@
 package hiof.mobilg11.quizapplication.model
 
+import kotlin.math.roundToInt
+
 data class PlayerStats(
     var username: String = "",
     var gamesPlayed: Int = 0,
     var gamesWon: Int = 0,
     var gamesLost: Int = 0,
     var gamesDraw: Int = 0,
-)
+) {
+    fun getWinPercentage(): Int {
+        if (gamesPlayed > 0) {
+            return ((gamesWon.toDouble() / gamesPlayed.toDouble()) * 100).roundToInt()
+        }
+        return 0
+    }
+}
+
